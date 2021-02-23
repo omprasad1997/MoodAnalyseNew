adc7d74e9f634d934aca2af52c76f161fed182f3
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-24 01:46:40 +0530

    Welcome

---
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,14 @@
+package com.moodanalyser;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("SAD");
+        Assert.assertEquals("SAD",mood);
+    }
+}
